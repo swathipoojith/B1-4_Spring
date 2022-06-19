cbf35d1cd79ff5310487fa5dea04be3aa1579429
Swathi <Swathi@DESKTOP-BVT9SUG>
2022-06-19 10:02:04 +0530

    First Commit

---
+++ SpringIOC/src/main/java/com/tns/springioc/Jio.java
@@ -0,0 +1,15 @@
+package com.tns.springioc;
+
+public class Jio implements Sim{
+
+	public void calling() {
+		System.out.println("Calling using Jio sim");
+		
+	}
+
+	public void data() {
+		System.out.println("data using Jio sim");
+		
+	}
+
+}
